4c7ce92b7eff7c35721dde0d84493de58eb748fc
Ana Laura Manera <analmanera@gmail.com>
2024-06-27 22:27:19 -0300

    index app

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const express = require("express");
 const app = express();
 const moviesRoutes = require("./routes/movies.router");
 
-const server = express();
-
 const PORT = process.env.PORT || 3000
 
-server.get("/", (req,res) => res.send("Hola movies en Alwaysdata!"));
+app.get("/", (req,res) => res.send("Hola movies en Alwaysdata!"));
 
-server.listen(PORT, () => console.log(`Servidor funcionando en http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Servidor funcionando en http://localhost:${PORT}`));
 
 app.use(express.json());
 app.use("/peliculas", require("./routes/movies.router"));
